19deb5a7f803cdfee05dee22c92bb5be6223d4bf
ATOM3S <51156529+ATOM3S@users.noreply.github.com>
2020-09-16 15:42:13 +0200

    ajout et modification des tags
    Tag Departement : suppression SECC et ajout dev
    Tag Application : modification de la regex
    Ajout du tag Projet

--- template.py
+++ template.py
@@ -12,5 +12,6 @@ tagTemplate = OrderedDict()
 
 tagTemplate['ID'] = "/"
 tagTemplate['Name'] = r"^[a-z0-9]+"
-tagTemplate['Departement'] = ["cloud", "secc"]
-tagTemplate['Application'] = r"^[a-z0-9]+"
+tagTemplate['Application'] = r"^[a-z0-9](-?[a-z0-9])*"
+tagTemplate['Departement'] = ["cloud", "ia", "dev"]
+tagTemplate['Projet'] = r"[A-Z][0-9]{5}"
